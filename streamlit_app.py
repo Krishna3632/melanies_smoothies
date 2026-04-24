0168cfe044a0661212547f5114173cc4a20252fd
Krishna Sharma <83634009+Krishna3632@users.noreply.github.com>
2026-04-24 15:08:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,8 @@ if ingredients_list:
 
     for fruit in ingredients_list:
         search_value = search_on_map.get(fruit, fruit)
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         try:
             url = f"https://fruityvice.com/api/fruit/{search_value}"
             response = requests.get(url)
